505bed0c42f22a589c0b71e972db3aa7f21f7bb1
pedrotemtem <peter.temtem123456789@gmail.com>
2024-03-04 22:40:50 +0000

    added new features

--- python.py
+++ python.py
@@ -14,39 +14,48 @@ apples = pd.read_csv('apple_quality.csv')
 # Remove leading and trailing whitespace from column names
 apples.columns = apples.columns.str.strip()
 
-#create new features
-#apples['Density'] = apples['Weight'] / apples['Size']
-#apples['SA Combo'] = apples['Acidity'] + apples['Sweetness']
-#apples['Texture'] = apples['Crunchiness'] + apples['Juiciness']
+# Create new features
+apples['Density'] = apples['Weight'] / apples['Size']
+apples['SA Combo'] = apples['Acidity'] + apples['Sweetness']
+apples['Texture'] = apples['Crunchiness'] + apples['Juiciness']
+apples['Size_Weight'] = apples['Size'] + apples['Weight']
+apples['Size_Juiciness'] = apples['Size'] + apples['Juiciness']
+apples['Juiciness_Sweetness'] = apples['Juiciness'] + apples['Sweetness']
+apples['Juiciness_Ripeness'] = apples['Juiciness']**2 / apples['Ripeness']**2
+apples['Size_Weight_Crunchiness'] = apples['Size'] * apples['Weight'] * apples['Crunchiness']
+apples['Sweetness_Acidity_Juiceness'] = (apples['Sweetness'] + apples['Acidity'] + apples['Juiciness'])/3
+apples['Overall_Texture'] = (apples['Sweetness'] + apples['Crunchiness'] + apples['Juiciness'] + apples['Ripeness'])/4
+apples['JS_SAJ'] = apples['Juiciness_Sweetness'] + apples['Sweetness_Acidity_Juiceness']
+apples['Crunchiness_Weight'] = apples['Crunchiness'] + apples['Weight']
 
+# Pairplot for overall distribution before making quality the last column
 sns.pairplot(apples, hue='Quality', plot_kws={'s': 5})
 plt.show()
 
-#making quality the last column
+# Making 'Quality' the last column
 cols = list(apples.columns.values)
 cols.pop(cols.index('Quality'))
-apples = apples[cols+['Quality']]
+apples = apples[cols + ['Quality']]
 
 # Standardize the data
 scaler = StandardScaler()
-standardized_data = scaler.fit_transform(apples.iloc[:, 1:-1])  # Exclude the A_id and Quality columns
+standardized_data = scaler.fit_transform(apples.iloc[:, :-1])  # Exclude the 'Quality' column
 
 # Normalize the data
 scaler = MinMaxScaler()
 normalized_data = scaler.fit_transform(standardized_data)
 
 # Convert the normalized data back to a DataFrame
-normalized_df = pd.DataFrame(normalized_data, columns=apples.columns[1:-1])
+normalized_df = pd.DataFrame(normalized_data, columns=apples.columns[:-1])
 
-# Add the Quality columns back to the DataFrame
+# Add the 'Quality' column back to the DataFrame
 normalized_df['Quality'] = apples['Quality']
 
-
-# Pairplot for overall distribution
+# Pairplot for overall distribution after standardization and normalization
 sns.pairplot(normalized_df, hue='Quality', plot_kws={'s': 5})
 plt.show()
 
-#plot histogram for each feature
+# Plot histogram for each feature
 plt.figure(figsize=(15, 8))
 for i, column in enumerate(normalized_df.columns[:-1], start=1):
     plt.subplot(2, 4, i)  # Adjust the subplot grid as needed
@@ -55,7 +64,7 @@ for i, column in enumerate(normalized_df.columns[:-1], start=1):
 plt.tight_layout()
 plt.show()
 
-#correlation matrix
+# Correlation matrix
 plt.figure(figsize=(10, 8))
 sns.heatmap(normalized_df.iloc[:, :-1].corr(), annot=False, cmap='coolwarm', fmt=".2f", linewidths=.5, square=True)
 plt.title('Correlation Matrix')
